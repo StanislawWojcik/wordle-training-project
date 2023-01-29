313adc98837be7bd41f2e19aa235a6f46421c77b
unknown <wojcikstw@gmail.com>
2023-01-29 20:57:12 +0100

    provide token wrapper object

---
+++ src/main/java/stasiek/wojcik/wordletrainingproject/entity/Token.java
@@ -0,0 +1,11 @@
+package stasiek.wojcik.wordletrainingproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Token {
+
+    private String token;
+}
