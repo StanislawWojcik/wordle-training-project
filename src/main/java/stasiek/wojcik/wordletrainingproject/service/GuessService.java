bc6d9fff72283ac9b0b26b66954a0cdd1489ac44
unknown <wojcikstw@gmail.com>
2023-05-13 19:47:00 +0200

    extend GameProgress object

--- src/main/java/stasiek/wojcik/wordletrainingproject/service/GuessService.java
+++ src/main/java/stasiek/wojcik/wordletrainingproject/service/GuessService.java
@@ -20,27 +20,21 @@ public class GuessService {
 
     public Optional<GuessResponse> processGuess(final String username, final String guess) {
         return repository.findUserByUsername(username)
-                .map(user -> processGuessForValidUser(user, guess));
+                .map(user -> isGuessValid(guess) && user.getGame() != null
+                        ? processExistingGame(user, guess)
+                        : null);
     }
 
-    private GuessResponse processGuessForValidUser(final User user, final String guess) {
-        return Optional.of(user.getGame())
-                .map(game -> {
-                    if (isGuessValid(guess)) {
-                        return processExistingGame(game, guess, user);
-                    } else return null;
-                })
-                .orElse(null);
-    }
-
-    private GuessResponse processExistingGame(final Game game, final String guess, final User user) {
+    private GuessResponse processExistingGame(final User user, final String guess) {
+        final var game = user.getGame();
         final var letterGuessResults = game.isGameValid()
-                ? processValidGame(game, guess, user)
+                ? processValidGame(user, guess)
                 : processGuess(game, game.getLastGuess());
         return new GuessResponse(game.getAttemptsCounter(), game.getStatus(), letterGuessResults, game.getKeyboard());
     }
 
-    private List<LetterGuessResult> processValidGame(final Game game, final String guess, final User user) {
+    private List<LetterGuessResult> processValidGame(final User user, final String guess) {
+        final var game = user.getGame();
         game.incrementAttemptsCounter();
         final var letterGuessResults = processGuess(game, guess);
         repository.save(user);
@@ -49,34 +43,36 @@ public class GuessService {
 
     private List<LetterGuessResult> processGuess(final Game game, final String guess) {
         final var word = game.getWord();
-        final var progress = new GameProgress();
+        final var progress = new GameProgress(word, guess);
         for (int i = 0; i < 5; i++) {
-            if (word.charAt(i) == guess.charAt(i)) {
-                validateCorrectLetter(progress, word, guess, i);
-            } else if (word.contains(String.valueOf(guess.charAt(i)))) {
-                validateIncorrectLetter(progress, word, guess, i);
+            progress.setIndex(i);
+            if (isLetterCorrect(progress)) {
+                validateCorrectLetter(progress);
+            } else if (isWordContainingLetter(progress)) {
+                validateIncorrectLetter(progress);
             } else {
                 progress.getAbsent().add(new LetterGuessResult(i, guess.charAt(i), LetterResult.ABSENT));
             }
         }
         updateStatusForWin(game, guess);
         game.setLastGuess(guess);
-        final var completeResultList =
-                buildCompleteList(progress.getCorrect(), progress.getMisplaced(), progress.getAbsent());
+        final var completeResultList = buildCompleteList(progress);
         updateKeyboard(completeResultList, game.getKeyboard());
         return completeResultList;
     }
 
-    private void validateCorrectLetter(final GameProgress progress, final String word, final String guess, final int index) {
+    private void validateCorrectLetter(final GameProgress progress) {
+        final var guess = progress.getGuess();
+        final var word = progress.getWord();
+        final var index = progress.getIndex();
         final var misplacedLetters = getLettersFromList(progress.getMisplaced(), guess.charAt(index));
-        final var correctLetters = getLettersFromList(progress.getCorrect(), guess.charAt(index));
-        final var misplacedLetter = misplacedLetters.stream().findFirst();
+        final var misplacedSpecificLetter = misplacedLetters.stream().findFirst();
         final var letterGuessResult = new LetterGuessResult(index, word.charAt(index), LetterResult.CORRECT);
-        if (misplacedLetter.isPresent()) {
-            if (!hasWordMoreLetters(misplacedLetters, correctLetters, word, word.charAt(index))) {
-                progress.getAbsent().add(new LetterGuessResult(misplacedLetter.get().index(),
+        if (misplacedSpecificLetter.isPresent()) {
+            if (!hasWordMoreLettersThanProgress(progress)) {
+                progress.getAbsent().add(new LetterGuessResult(misplacedSpecificLetter.get().index(),
                         guess.charAt(index), LetterResult.ABSENT));
-                progress.getMisplaced().remove(misplacedLetter.get());
+                progress.getMisplaced().remove(misplacedSpecificLetter.get());
             }
             progress.getCorrect().add(letterGuessResult);
         } else {
@@ -84,10 +80,10 @@ public class GuessService {
         }
     }
 
-    private void validateIncorrectLetter(final GameProgress progress, final String word, final String guess, final int index) {
-        final var misplacedLetters = getLettersFromList(progress.getMisplaced(), guess.charAt(index));
-        final var correctLetters = getLettersFromList(progress.getCorrect(), guess.charAt(index));
-        if (hasWordMoreLetters(misplacedLetters, correctLetters, word, guess.charAt(index))) {
+    private void validateIncorrectLetter(final GameProgress progress) {
+        final var guess = progress.getGuess();
+        final var index = progress.getIndex();
+        if (hasWordMoreLettersThanProgress(progress)) {
             progress.getMisplaced().add(new LetterGuessResult(index, guess.charAt(index),
                     LetterResult.INCORRECT_POSITION));
         } else {
@@ -95,19 +91,30 @@ public class GuessService {
         }
     }
 
-    private boolean hasWordMoreLetters(final List<LetterGuessResult> misplaced, final List<LetterGuessResult> correct,
-                                       final String word, final char letter) {
-        return misplaced.size() + correct.size() < StringUtils.countOccurrencesOf(word, String.valueOf(letter));
+    private boolean isWordContainingLetter(final GameProgress progress) {
+        return progress.getWord().contains(String.valueOf(progress.getGuess().charAt(progress.getIndex())));
+    }
+
+    private boolean isLetterCorrect(final GameProgress progress) {
+        return progress.getWord().charAt(progress.getIndex()) == progress.getGuess().charAt(progress.getIndex());
+    }
+
+    private boolean hasWordMoreLettersThanProgress(final GameProgress progress) {
+        final var guess = progress.getGuess();
+        final var word = progress.getWord();
+        final var index = progress.getIndex();
+        final var misplacedLetters = getLettersFromList(progress.getMisplaced(), guess.charAt(index));
+        final var correctLetters = getLettersFromList(progress.getCorrect(), guess.charAt(index));
+        return misplacedLetters.size() + correctLetters.size()
+                < StringUtils.countOccurrencesOf(word, String.valueOf(guess.charAt(index)));
     }
 
     private List<LetterGuessResult> getLettersFromList(final List<LetterGuessResult> letters, final char letterAtIndex) {
         return letters.stream().filter(letter -> letter.letter().equals(letterAtIndex)).toList();
     }
 
-    private List<LetterGuessResult> buildCompleteList(final List<LetterGuessResult> correct,
-                                                      final List<LetterGuessResult> incorrectPosition,
-                                                      final List<LetterGuessResult> absent) {
-        return Stream.of(correct, incorrectPosition, absent)
+    private List<LetterGuessResult> buildCompleteList(final GameProgress progress) {
+        return Stream.of(progress.getCorrect(), progress.getMisplaced(), progress.getAbsent())
                 .flatMap(Collection::stream)
                 .sorted(Comparator.comparing(LetterGuessResult::index))
                 .toList();
--- src/test/java/stasiek/wojcik/wordletrainingproject/controller/BaseIntegrationTest.java
+++ src/test/java/stasiek/wojcik/wordletrainingproject/controller/BaseIntegrationTest.java
@@ -1,15 +1,23 @@
 package stasiek.wojcik.wordletrainingproject.controller;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public class BaseIntegrationTest {
 
-    final MongoDBContainer mongoDBContainer =
-            new MongoDBContainer(DockerImageName.parse("mongo:5.0.16"));
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.13");
 
+    static {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 }
